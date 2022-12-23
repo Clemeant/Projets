0dc526b3baf79911d7a628f7fba63503f0d1334b
Clement Saint-Hilaire <113097056+Clemeant@users.noreply.github.com>
2022-12-23 17:21:30 +0100

    Update 2-apropos.php

--- portefolioC/php/2-apropos.php
+++ portefolioC/php/2-apropos.php
@@ -5,7 +5,7 @@ $description = $data['description'];
 
 <section class="about" id="apropos">
     <div class="main" >
-        <img src="images/image.png">
+        <img src="images/Image.png">
             <div class="about-text">
               <h2><?php echo $description['1'] ?></h2>
               <h5><?php echo $description['2'] ?><span><?php echo $description['3'] ?></span></h5>
